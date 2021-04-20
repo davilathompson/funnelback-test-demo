cb88e8e4d012b1e6e5a39053e933e2f1b712040c
davilathompson <davila.thompson@gmail.com>
2021-04-20 12:02:42 +1000

    1 file modified

--- src/events/eventTileItem.js
+++ src/events/eventTileItem.js
@@ -20,12 +20,6 @@ export default function EventTileItem(re) {
         {imageStyle(d)}
         <div className="card-body">
           <h5 className="card-title"> {d.title}</h5>
-          <p className="card-text">
-            <span className="boxes-event__day">{UnixToDayCalc(d.date)} </span>
-            <span className="boxes-event__month">
-              {UnixToMonthCalc(d.date)}
-            </span>
-          </p>
           <a href={d.clickTrackingUrl} className="card-link">
             read more
           </a>
